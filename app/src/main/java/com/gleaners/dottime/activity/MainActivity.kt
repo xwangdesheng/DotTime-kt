1148ec0a4adfd2f57b04fc7090a7e263d7624335
wangdesheng <wangdesheng2017@163.com>
2021-04-23 16:34:08 +0800

    git init..

---
+++ app/src/main/java/com/gleaners/dottime/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gleaners.dottime.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gleaners.dottime.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
